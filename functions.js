4e29c98440a91938ed5c9e05049cf44f46b39afc
danipreciado <danielapca96@gmail.com>
2023-05-30 07:22:49 -0700

    recursive reading of dir, returns promise that resolves into an array of objects containg links

--- functions.js
+++ functions.js
@@ -4,24 +4,78 @@ const path = require('path')
 const absolutePath = (userPath) => path.isAbsolute(userPath);
 const resolvePath = (userPath) => path.resolve(userPath);
 const extMd = (userPath) => path.extname(userPath) === '.md';
-const isDir = (userPath, callback) => {
-  fs.stat(userPath, (err, stats) => {
-    if (err) {
-      return callback(err);
-    }
 
-    if (stats.isDirectory()) {
-      
-      return callback(null, true);
-      
-    } else {
-      return callback(null, false);
-    }
+function isDir(userPath) {
+  return new Promise((resolve, reject) => {
+    fs.stat(userPath, (err, stats) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(stats.isDirectory());
+    });
   });
-};
+}
+
 function filterFiles(files, extension) {
   return files.filter(file => path.extname(file) === extension);
 }
+ 
+function findFilesInDir(directory, extension = '.md') {
+    const files = fs.readdirSync(directory);
+    let filteredFiles = [];
+  
+    files.forEach(file => {
+      const filePath = path.join(directory, file);
+      const stats = fs.statSync(filePath);
+  
+      if (stats.isDirectory()) {
+        const subDirectoryFiles = findFilesInDir(filePath, extension);
+        filteredFiles = filteredFiles.concat(subDirectoryFiles);
+      } else if (path.extname(file) === extension) {
+          filteredFiles.push(filePath);
+      }
+    });
+  
+    return filteredFiles;
+  }
+
+  function readFile(filePath) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve(data);
+      });
+    });
+  }
+  
+  function findLinksInContent(content, filePath) {
+    const linkRegex = /\[([^\]]+)\]\(([^\)]+)\)/g;
+    const links = [];
+    let match;
+  
+    while ((match = linkRegex.exec(content)) !== null) {
+      const linkText = match[1];
+      const linkUrl = match[2];
+      links.push({ href: linkUrl, text: linkText, file: filePath  });
+    }
+  
+    return links;
+  }
+
+module.exports = {
+  absolutePath,
+  resolvePath, 
+  extMd,
+  findFilesInDir,
+  isDir,
+  readFile, 
+  findLinksInContent,
+};
 
 /* function findFilesInDir(directory, extension = '.md', callback) {
  
@@ -47,60 +101,3 @@ function filterFiles(files, extension) {
       callback(null, filteredFiles);
     });
   }; */ 
- 
-  function findFilesInDir(directory, extension = '.md') {
-    try {
-      const files = fs.readdirSync(directory);
-      let filteredFiles = [];
-  
-      files.forEach(file => {
-        const filePath = path.join(directory, file);
-        const stats = fs.statSync(filePath);
-  
-        if (stats.isDirectory()) {
-          const subDirectoryFiles = findFilesInDir(filePath, extension);
-          filteredFiles = filteredFiles.concat(subDirectoryFiles);
-        } else if (path.extname(file) === extension) {
-          filteredFiles.push(filePath);
-        }
-      });
-      return filteredFiles;
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  function readFile(filePath, callback) {
-    
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        return callback(err);
-      }
-  
-      callback(null, data);
-    });
-  }
-  
-  function findLinksInContent(content) {
-    const linkRegex = /\[([^\]]+)\]\(([^\)]+)\)/g;
-    const links = [];
-    let match;
-  
-    while ((match = linkRegex.exec(content)) !== null) {
-      const linkText = match[1];
-      const linkUrl = match[2];
-      links.push({ text: linkText, url: linkUrl });
-    }
-  
-    return links;
-  }
-
-module.exports = {
-  absolutePath,
-  resolvePath, 
-  extMd,
-  findFilesInDir,
-  isDir,
-  readFile, 
-  findLinksInContent,
-};
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
-const { absolutePath, resolvePath, extMd, findFilesInDir, isDir, readFile, findLinksInContent } = require('./functions.js')
-const path = require('path');
+const { absolutePath, resolvePath, findFilesInDir, isDir, readFile, findLinksInContent } = require('./functions.js')
 
 const userPath = process.argv[2];
 const extension = process.argv[3];
@@ -11,42 +10,23 @@ isDir(resolvedUserPath)
     if (isDirectory) {
       return findFilesInDir(resolvedUserPath, extension);
     } else {
-      throw new Error('The specified path is not a directory.');
+      return [resolvedUserPath]; // Treat it as a single file
     }
   })
   .then((files) => {
-    console.log(files);
-  })
-  .catch
-
-
-/* 
-isDir(resolvedUserPath, (err, isDirectory) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  if (isDirectory) {
-    findFilesInDir(resolvedUserPath, ext  => {
-   
-      files.forEach(file => {
-        
-        
-        readFile(file, (err, content) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log(content);
-          });
-      }); 
+    const promises = files.map((file) => {
+      return readFile(file).then((content) => {
+        const links = findLinksInContent(content, file);
+        return links;
+      });
     });
-  } else {
-    if (extMd(resolvedUserPath)){
-      console.log('file is md')
-    } else {
-      console.log('file is not md')
-    }
-  }
-}); */
+
+    return Promise.all(promises);
+  })
+  .then((results) => {
+    const flattenedLinks = results.flat(); // It was giving me multiple arrays, this converts it into a single array, multiple objs
+    console.log(flattenedLinks);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
